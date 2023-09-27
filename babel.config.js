8bc5353d9cc1fa8f33b14a2a33eabe45bb30842b
Cody Marshall <codymstuntsx@gmail.com>
2023-09-27 09:43:07 -0700

    init commit

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,15 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ["babel-preset-expo"],
+    env: {
+      production: {
+        plugins: ["react-native/babel"],
+      },
+    },
     plugins: [
       // Required for expo-router
-      'expo-router/babel',
+      "expo-router/babel",
     ],
   };
 };
