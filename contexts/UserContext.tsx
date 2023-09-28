13accccdb93928d10d1d146fc7118983bf09175e
Cody Marshall <codymstuntsx@gmail.com>
2023-09-27 20:38:12 -0700

    CREATED: USER context

---
+++ contexts/UserContext.tsx
@@ -0,0 +1,34 @@
+// UserProfileContext.tsx
+import React, { createContext, useContext, useState, ReactNode } from "react";
+import { USER_PROFILE, UserProfile } from "../data/Test";
+
+interface UserProfileContextType {
+  userProfile: UserProfile | null;
+  setUserProfile: React.Dispatch<React.SetStateAction<UserProfile | null>>;
+}
+
+const UserProfileContext = createContext<UserProfileContextType | undefined>(
+  undefined
+);
+
+export const UserProfileProvider: React.FC<{ children: ReactNode }> = ({
+  children,
+}) => {
+  const [userProfile, setUserProfile] = useState<UserProfile | null>(
+    USER_PROFILE
+  );
+
+  return (
+    <UserProfileContext.Provider value={{ userProfile, setUserProfile }}>
+      {children}
+    </UserProfileContext.Provider>
+  );
+};
+
+export const useUserProfile = (): UserProfileContextType => {
+  const context = useContext(UserProfileContext);
+  if (!context) {
+    throw new Error("No User Provided");
+  }
+  return context;
+};
