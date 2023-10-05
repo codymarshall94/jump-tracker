b8998d194e26ca6e61813cb7d4bdec267f43737a
Cody Marshall <codymstuntsx@gmail.com>
2023-10-04 21:19:14 -0700

    RENAMED: to get started from index

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -3,7 +3,7 @@ import { Stack } from "expo-router";
 export default function AuthLayout() {
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: false }} />
+      <Stack.Screen name="get-started" options={{ headerShown: false }} />
       <Stack.Screen name="sign-in" options={{ headerShown: false }} />
       <Stack.Screen name="sign-up" options={{ headerShown: false }} />
     </Stack>
---
+++ app/(auth)/get-started.tsx
@@ -0,0 +1,5 @@
+import GetStarted from "../../screens/auth/get-started/GetStarted";
+
+export default function EntranceAuth() {
+  return <GetStarted />;
+}
