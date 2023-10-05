f6dd41fd1616ccd5f9977b9557acc7007a5e2043
Cody Marshall <codymstuntsx@gmail.com>
2023-10-04 21:19:41 -0700

    UPDATED: removed test day and brought in user jump

--- components/line-chart/LineChart.tsx
+++ components/line-chart/LineChart.tsx
@@ -1,67 +1,72 @@
-"use client";
-
-import { useUserProfile } from "../../contexts/UserContext";
-import { Dimensions, View } from "react-native";
+import { Dimensions, View, Text } from "react-native";
 import { LineChart } from "react-native-chart-kit";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import { useTheme } from "react-native-paper";
+import { useAuthenticatedUser } from "../../contexts/AuthContext";
 
 export default function LineChartData() {
   const theme = useTheme();
-  const [selectedData, setSelectedData] = useState("broad_jump");
-  const { userProfile } = useUserProfile();
+  const [selectedData, setSelectedData] = useState<string>("broad_jump");
+  const { userProfile, loading } = useAuthenticatedUser();
+  const [chartData, setChartData] = useState<any>(null);
 
   const screenWidth = Dimensions.get("window").width;
 
-  if (userProfile) {
-    const filteredSessions = userProfile.jumpSessions.filter((session) => {
-      const hasSelectedJump = session.jumps.some(
-        (jump) => jump.jumpId === selectedData
+  console.log(userProfile);
+
+  useEffect(() => {
+    if (userProfile?.jumpSessions && selectedData) {
+      const filteredJumpSessions = userProfile.jumpSessions.filter(
+        (session) => session.jumpId === selectedData
       );
-      return hasSelectedJump;
-    });
 
-    const xLabels = filteredSessions.map((session) => session.date);
-    const yLabel = filteredSessions.map((session) => {
-      const jump = session.jumps.find((jump) => jump.jumpId === selectedData);
-      return jump ? jump.distance : 0;
-    });
+      const data = {
+        labels: [],
+        datasets: [
+          {
+            data: filteredJumpSessions.map(
+              (session) => session.sessionHighestJump
+            ),
+          },
+        ],
+      };
 
-    const data = {
-      labels: xLabels,
-      datasets: [
-        {
-          data: yLabel,
-        },
-      ],
-    };
-    const chartConfig = {
-      backgroundColor: theme.colors.onSecondary,
-      backgroundGradientFrom: theme.colors.onSecondary,
-      backgroundGradientTo: theme.colors.onSecondary,
-      color: (opacity = 1) => `rgba(107, 79, 170, ${opacity})`,
-      labelColor: (opacity = 1) => {
-        return `rgba(${theme.colors.onBackground}, ${opacity})`;
-      },
-      strokeWidth: 2, // optional, default 3
-      barPercentage: 0.5,
-      decimalPlaces: 0,
-      useShadowColorFromDataset: false,
-    };
+      setChartData(data);
+    }
+  }, [userProfile, selectedData]);
 
-    return (
-      <LineChart
-        data={data}
-        width={screenWidth}
-        height={220}
-        chartConfig={chartConfig}
-        yAxisSuffix="in"
-        withHorizontalLines={false}
-        withVerticalLines={false}
-        bezier={true}
-      />
-    );
+  if (loading) {
+    return <Text>Loading...</Text>;
   }
 
-  return <View>Loading...</View>;
+  if (!chartData) {
+    return <Text>No data available for the selected jump.</Text>;
+  }
+
+  const chartConfig = {
+    backgroundColor: theme.colors.onSecondary,
+    backgroundGradientFrom: theme.colors.onSecondary,
+    backgroundGradientTo: theme.colors.onSecondary,
+    color: (opacity = 1) => `rgba(107, 79, 170, ${opacity})`,
+    labelColor: (opacity = 1) => {
+      return `rgba(${theme.colors.onBackground}, ${opacity})`;
+    },
+    strokeWidth: 2,
+    barPercentage: 0.5,
+    decimalPlaces: 0,
+    useShadowColorFromDataset: false,
+  };
+
+  return (
+    <LineChart
+      data={chartData}
+      width={screenWidth}
+      height={220}
+      chartConfig={chartConfig}
+      yAxisSuffix="in"
+      withHorizontalLines={false}
+      withVerticalLines={false}
+      bezier={true}
+    />
+  );
 }
