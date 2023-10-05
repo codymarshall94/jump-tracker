e083ff65a5bc234a9885fd77134798f4138eb629
Cody Marshall <codymstuntsx@gmail.com>
2023-10-04 21:12:15 -0700

    REMOVED

--- app/(auth)/index.tsx
+++
@@ -1,5 +0,0 @@
-import GetStarted from "../../screens/auth/get-started/GetStarted";
-
-export default function EntranceAuth() {
-  return <GetStarted />;
-}
--- app/settings/index.tsx
+++
@@ -1,5 +0,0 @@
-import Settings from "../../screens/settings/Settings";
-
-export default function SettingsPage() {
-  return <Settings />;
-}
